2ae1893925f4e9dd6aa4e54486880941ba998f56
gopikrishnayakkati <gopikrishna0527@gmail.com>
2025-01-29 17:47:52 +0530

    Docker install script

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,15 +80,14 @@ resource "aws_instance" "react_native_notes" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update -y",
-      "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
-      "sudo apt-get update -y",
-      "sudo apt-get install -y docker-ce",
+      "curl -fsSL https://get.docker.com -o install-docker.sh",
+      "sudo sh install-docker.sh",
+      "sudo docker info",
       "git clone https://github.com/aayush301/react-native-notes-app.git",
       "cd react-native-notes-app",
       "sudo docker build -t react-native-notes .",
-      "sudo docker run -p 8081:8081 react-native-notes"
+      "sudo docker run -d -p 8081:8081 react-native-notes",
+      "sudo docker container ls -a"
     ]
 
     connection {
